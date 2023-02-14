c7a553ecc73a9a0f8e40ccf31f7e23b0f3d55e17
siqi.ran <rsq-work@outlook.com>
2023-02-14 11:09:30 +0800

    style: change the scope of some fields.

--- src/main/java/org/learning/VavrFunction.java
+++ src/main/java/org/learning/VavrFunction.java
@@ -9,15 +9,14 @@ import io.vavr.control.Option;
 public class VavrFunction {
 
     Function2<Integer, Integer, Integer> sum = (a, b) -> a + b;
-    Function1<Integer, Integer> plusOne = a -> a + 1;
-    Function1<Integer, Integer> multiplyByTwo = a -> a * 2;
-    Function2<Integer, Integer, Integer> divide = (a, b) -> a / b;
 
     private VavrFunction() {
 
     }
 
     public static Integer composition() {
+        Function1<Integer, Integer> plusOne = a -> a + 1;
+        Function1<Integer, Integer> multiplyByTwo = a -> a * 2;
         /* todo: use plusOne and multiplyByTwo to compose a new function which can add one to the input
         then multiply by two as return value.
         use compose() to a finish this.*/
@@ -26,6 +25,7 @@ public class VavrFunction {
 
 
     public static Function2<Integer, Integer, Option<Integer>> safeDivide() {
+        Function2<Integer, Integer, Integer> divide = (a, b) -> a / b;
         /* todo: convert the initial divide function to a total function which will accept infeasible parameters
         and return None or Some according to the input.
         use lift() to a finish this.*/
@@ -34,7 +34,6 @@ public class VavrFunction {
 
 
     public static Function1<Integer, Integer> addTen() {
-
         Function3<Integer, Integer, Integer, Integer> sumByThreeNumbers = (a, b, c) -> a + b + c;
         /* todo: convert the sumByThreeNumbers function to a function which accepts one parameter and behave
         as adding ten to the input.
