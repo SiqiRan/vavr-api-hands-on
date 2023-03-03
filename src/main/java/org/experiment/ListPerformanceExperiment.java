4638c7a1fbded38ad55d6f5777ebe3131ef0b11d
SiqiRan <rsq-work@outlook.com>
2023-03-03 14:09:51 +0800

    feat: compare the insertion between vavr list, java array list and linked list.

--- src/main/java/org/experiment/ListPerformanceExperiment.java
+++ src/main/java/org/experiment/ListPerformanceExperiment.java
@@ -13,40 +13,80 @@ import java.util.LinkedList;
 @Getter
 @Setter
 public class ListPerformanceExperiment {
-    private List<String> vavrList = List.of("one", "two", "three");
     private java.util.List<String> arrayList = new ArrayList<>(Arrays.asList("one", "two", "three"));
     private java.util.List<String> linkedList = new LinkedList<>(Arrays.asList("one", "two", "three"));
-
     private static Logger logger = LogManager.getLogger(ListPerformanceExperiment.class);
 
-    public void insertionEfficiency() {
-        long n = 1000;
+    String middle = "middle";
+    String head = "head";
+    String tail = "tail";
+
+    public void performance() {
+        vavrListCalculateTime(10000, head);
+        vavrListCalculateTime(10000, middle);
+        vavrListCalculateTime(10000, tail);
+        javaListCalculateTime(10000, head, arrayList);
+        javaListCalculateTime(10000, middle, arrayList);
+        javaListCalculateTime(10000, tail, arrayList);
+        javaListCalculateTime(10000, head, linkedList);
+        javaListCalculateTime(10000, middle, linkedList);
+        javaListCalculateTime(10000, tail, linkedList);
+    }
+
+    private void vavrListCalculateTime(long times, String position) {
+        List<String> vavrList = List.of("one", "two", "three");
         long startTime = System.nanoTime();
-        //10000 times insertion in vavrList
-        for (int i = 0; i < n; i++) {
-            vavrList = vavrList.append("ok");
-        }
+        vavrListExecution(times, vavrList, position);
         long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
-        logger.info("VavrList " + totalTime);
-
-        startTime = System.nanoTime();
-        for (int i = 0; i < n; i++) {
-            int floorHalf = Math.floorDiv(i, 2);
-            arrayList.add(floorHalf, "ok");
-        }
-        endTime = System.nanoTime();
-        totalTime = endTime - startTime;
-        logger.info("ArrayList " + totalTime);
-        //10000 times insertion in javaList
-
-        startTime = System.nanoTime();
-        for (int i = 0; i < n; i++) {
-            int floorHalf = Math.floorDiv(i, 2);
-            linkedList.add(floorHalf, "ok");
-        }
-        endTime = System.nanoTime();
-        totalTime = endTime - startTime;
-        logger.info("LinkedList " + totalTime);
+        logger.info(vavrList.getClass() + " " + position + " " + totalTime);
     }
+
+    private void javaListCalculateTime(long times, String position, java.util.List<String> javaList) {
+        long startTime = System.nanoTime();
+        javaListExecution(times, javaList, position);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        logger.info(javaList.getClass() + " " + position + " " + totalTime);
+    }
+
+    private static void vavrListExecution(long times, List<String> vavrList, String position) {
+        for (long i = 0; i < times; i++) {
+            switch (position) {
+                case "head":
+                    vavrList = vavrList.push("new");
+                    break;
+                case "middle":
+                    int half = (int) Math.floorDiv(i, 2);
+                    vavrList = vavrList.insert(half, "new");
+                    break;
+                case "tail":
+                    vavrList = vavrList.append("new");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void javaListExecution(long times, java.util.List<String> javaList, String position) {
+        for (long i = 0; i < times; i++) {
+            switch (position) {
+                case "head":
+                    javaList.add(0, "new");
+                    break;
+                case "middle":
+                    int half = (int) Math.floorDiv(i, 2);
+                    javaList.add(half, "new");
+                    break;
+                case "tail":
+                    javaList.add("new");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 }
+
