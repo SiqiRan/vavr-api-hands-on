fdb014f840ca216c789cb0990c55106412299085
SiqiRan <rsq-work@outlook.com>
2023-03-04 15:22:19 +0800

    fix: fix some unhandled conditions in Execution.

--- src/test/java/org/dataStructures/PerformanceExperimentTest.java
+++ src/test/java/org/dataStructures/PerformanceExperimentTest.java
@@ -1,21 +1,29 @@
 package org.dataStructures;
 
 import org.experiment.Execution;
-import org.experiment.PerformanceExperiment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class PerformanceExperimentTest {
 
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    ArrayList<Object> emptyList = new ArrayList<>();
+    ArrayList<Object> headList = new ArrayList<>(Collections.singletonList(0));
+    ArrayList<Object> twoParamsList = new ArrayList<>(Arrays.asList(0, 1));
+    List<Execution> executionList = new ArrayList<>();
+    io.vavr.collection.List<Integer> vavrList = io.vavr.collection.List.of();
+
+    long TIMES = 1000;
+
     @BeforeEach
     void setUp() {
+        executionList.add(new Execution("insert head", headList, arrayList, TIMES));
+        executionList.add(new Execution("insert middle", twoParamsList, arrayList, TIMES));
+        executionList.add(new Execution("insert tail", twoParamsList, arrayList, TIMES));
     }
 
     @AfterEach
@@ -24,8 +32,8 @@ class PerformanceExperimentTest {
 
     @Test
     void testFunctionalities() throws InvocationTargetException, IllegalAccessException {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        Execution execution = new Execution("insert head", new ArrayList<>(Arrays.asList(1)), arrayList, 1000);
-        execution.execute();
+        for (Execution execution : executionList) {
+            execution.execute();
+        }
     }
 }
